d0749116b3519f7d8e3932fc143f1c584f3230c7
Haris Ali <s2023065049@umt.edu.pk>
2026-07-04 15:02:25 +0500

    streamlit addition

--- config.py
+++ config.py
@@ -1,14 +1,21 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+# First try Streamlit Secrets, then fall back to .env
+HUGGINGFACEHUB_API_TOKEN = st.secrets.get(
+    "HUGGINGFACEHUB_API_TOKEN",
+    os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
 
-CHATBOT_NAME = os.getenv("CHATBOT_NAME", "HarisBot")
+CHATBOT_NAME = st.secrets.get(
+    "CHATBOT_NAME",
+    os.getenv("CHATBOT_NAME", "HarisBot")
+)
 
 DATA_PATH = "data/"
-
 FAISS_INDEX_PATH = "faiss_index/"
 
 CHUNK_SIZE = 500
